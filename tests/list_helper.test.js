ba9712e28e59f6d655bcfce7ecfeb3b890c03dbd
Dariush Bahri <dazb@alumni.york.ac.uk>
2020-09-12 14:30:24 +0800

    4.4: helper functions and unit tests, step2

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -6,3 +6,55 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const listWithManyBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: 'fdsf',
+      title: 'bum',
+      author: 'bum',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: 'dsada',
+      title: 'fdsfsdf',
+      author: 'bufdsfsdfm',
+      url: 'hfdsfdsfl',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  test('when list has only one blog, equals the likes of that', () => {
+    expect(listHelper.totalLikes(listWithOneBlog)).toBe(5)
+  })
+
+  test('when list has many blogs, equals combined likes', () => {
+    expect(listHelper.totalLikes(listWithManyBlogs)).toBe(20)
+  })
+
+  test('when list has no blogs, equals 0', () => {
+    expect(listHelper.totalLikes([])).toBe(0)
+  })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const likesReducer = (sum, blogs) => {
+    return sum + blogs.likes
+  }
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce(likesReducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
